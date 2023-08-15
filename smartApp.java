4d89931d221ad7cef21a7b8af7fabb11798c4a67
Ganindu Shakthi <ganindu2shakthi@gmail.com>
2023-08-15 22:56:41 +0530

    Created Constants

---
+++ smartApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class smartApp{
+    private final static Scanner scaneer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "Welcome to Smart Banking";
+        final String CREATE_NEW_ACCOUNT = "Create New Account";
+       
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        
+    }
+}
