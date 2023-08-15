12e9577ce27d12e740d14592cf5c72b1fcc9ffca
Ganindu Shakthi <ganindu2shakthi@gmail.com>
2023-08-15 23:00:41 +0530

    Dashboard completed

--- smartApp.java
+++ smartApp.java
@@ -17,6 +17,48 @@ public class smartApp{
         final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
         final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
 
+        String screen = DASHBOARD;
+
+        do{
+            final String APP_TITLE = String.format("%s%s%s",
+            COLOR_BLUE_BOLD, screen, RESET);
+
+            String[] id = new String[0];
+            String[] name = new String[0];
+            Double[] accBalance = new Double[0]; 
+
+            System.out.println(CLEAR);
+            System.out.println("\t" + APP_TITLE + "\n");
+
+            switch ((screen)) {
+                case DASHBOARD:
+
+                    System.out.println("\t[1] Create New Account");
+                    System.out.println("\t[2] Deposits");
+                    System.out.println("\t[3] Withdrawals");
+                    System.out.println("\t[4] Transfer");
+                    System.out.println("\t[5] Print Statement");
+                    System.out.println("\t[6] Delete Account");
+                    System.out.println("\t[7] Exit");
+                    System.out.print("\tEnter an option to continue: ");
+                    int option = scaneer.nextInt();
+                    scaneer.nextLine();
+                   
+                case CREATE_NEW_ACCOUNT:
+                    
+
+
+                    break;
+
+
+
+                    
+            
+                default:
+                    break;
+            }
+
+        }while(true);
         
     }
 }
